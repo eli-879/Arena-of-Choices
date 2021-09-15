94bd3e5ceb80576766199657c8fde1b4650d3681
Eric Li <ericxli2001@gmail.com>
2021-09-15 23:15:01 +1200

    changed health back to 100

--- src/character.js
+++ src/character.js
@@ -24,7 +24,7 @@ export default class Character {
 
         // character attributes
         this.maxHealth = 100;
-        this.health = 20;
+        this.health = 100;
         this.dmg = 10;
     }
 
