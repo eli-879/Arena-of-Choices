7d4264197dbbfe826772ee13d2ebddfc55c0e354
Eric Li <ericxli2001@gmail.com>
2021-09-24 14:11:20 +1200

    edited npm details

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const Datastore = require('nedb');
 
 const app = express();
+const port = process.env.PORT || 3000;
 app.listen(3000, () => console.log("IM LISTENING"));
 
 app.use(express.static('public'));
