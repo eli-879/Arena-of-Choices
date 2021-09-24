723d764ba90af6983da4ba6fa3b3be4d3bc7135f
Eric Li <ericxli2001@gmail.com>
2021-09-24 19:22:40 +1200

    restructure files

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const Datastore = require('nedb');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log("IM LISTENING"));
 
-app.use(express.static('public'));
-app.use(express.json({limit: '1mb'}));
+app.use(express.static('public'))
+
+
 
 const database = new Datastore("database.db");
 database.loadDatabase();
