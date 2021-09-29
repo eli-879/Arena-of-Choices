44a8a36dc7b5c9a154a8ab04d38f1080f5b0bac1
Eric Li <ericxli2001@gmail.com>
2021-09-29 17:50:51 +1300

    responsiveness upgrades

---
+++ public/scripts/scripts.js
@@ -0,0 +1,8 @@
+const toggleButton = document.getElementsByClassName("toggle-button")[0];
+const navbarLinks = document.getElementsByClassName("navbar-links")[0];
+
+toggleButton.addEventListener("click", () => {
+	console.log("D");
+	console.log(navbarLinks.classList);
+	navbarLinks.classList.toggle("active");
+});
